1ac212ed89977f3679d07ae96b24054a03b4d50b
unknown <bkt5031@gmail>
2020-08-12 22:52:07 -0400

    post ajax works

--- djangoRestVgs/urls.py
+++ djangoRestVgs/urls.py
@@ -1,18 +1,3 @@
-"""djangoRestVgs URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.conf.urls import include, url
 from django.urls import path
@@ -20,15 +5,27 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from vgsRestApp import views
 from django.views.generic.base import TemplateView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    url(r'^ccGet/', views.ccGet.as_view(), name='ccGet'),
-    url(r'^ccPost/', views.ccPost.as_view(), name='ccPost'),
-    url(r'^index/', views.index.as_view(), name='index'),
+    # url(r'^index/', views.index.as_view(), name='index'),
+    # url(r'^ccGet/', views.ccGet.as_view(), name='ccGet'),
+    # url(r'^ccPost/', views.ccPost.as_view(), name='ccPost'),
+    path('index/', views.index.as_view(), name='index'),
+    # path('index/', views.index, name='index'),
+    path('ccPost/',views.ccPost.as_view(), name='ccPost'),
+    path('ccGet/', views.ccGet.as_view(), name='ccGet'),
     # url(r'^/', views.insertCC.as_view(), name='ccinsert'),
     # path('index/', views.insertCC)
-  
+
 ]
 
+
+# urlpatterns = [
+# path('admin/', admin.site.urls),
+# path('index/', views.index, name='index'),
+# path('ccPost/', views.ccPost, name='ccPost'),
+# path('ccGet/', views.index, name='ccGet'),
+# ]
+#
 urlpatterns = format_suffix_patterns(urlpatterns)
--- vgsRestApp/views.py
+++ vgsRestApp/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render, redirect, HttpResponse
+
+from .forms import CreditCardForm
 from django.shortcuts import render
 
 # Create your views here.
@@ -17,21 +20,33 @@ from django.views.decorators.csrf import csrf_exempt
 from django.template import loader
 from rest_framework import status
 from django.shortcuts import render
-# @api_view(['GET'])
-# def post(self, request):
-#         if request.method == "POST":
-#             a = request.POST.get('tag', 'default')
-#             print("printing", a)
-#             print(request.POST)
-#         return HttpResponse("Hello")
+
+def index(self, request):
+    if request.method == 'POST':
+        form = CreditCardForm(request.POST)
+    if form.is_valid():
+        form.save()
+# return redirect('/ccPost/')
+        return HttpResponse("data submitted successfully")
+
+
+    else:
+        form = CreditCardForm()
+
+        return render(request, 'index.html', {'form': form})
+
+
+def ccPost(request):
+
+    return HttpResponse("data submitted successfully")
+
 
 class index(APIView):
-    
+
     def get(self, request):
 
         return render(request, 'index.html')
 
-    
 
 class ccGet(APIView):
     def get(self, request):
@@ -41,17 +56,19 @@ class ccGet(APIView):
         return Response(retData)
         # return retData["json"]
 
+
 class ccPost(APIView):
-    def post(self, request):
+
+    def post(self, request): 
         # creditCard1 = creditCardInfo.objects.all()
         serializer = creditCardInfoSerializer(data=request.data)
-        
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-            # return render(request, 'index.html', {'response': response
+# return Response(serializer.data, status=status.HTTP_201_CREATED)
+# return render(request, 'index.html', {'response': response
+            return redirect('/ccGet/')
 
-                                            #   })
+# })
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # def insertCC(request):
@@ -65,13 +82,13 @@ class ccPost(APIView):
 #         return render(request, 'index.html')
 #     else:
 #         return render(request, 'index.html')
-#inbound   
+# inbound
 # response = requests.post("https://tntsfeqzp4a.sandbox.verygoodproxy.com/post/",
-        #                   json={'cc_number': 'card_number',
-        #                   'cc_exp': 'exp_date',raise_exception=True
-        #                   'cc_cvv': 'cvv'})response
+#                   json={'cc_number': 'card_number',
+#                   'cc_exp': 'exp_date',raise_exception=True
+#                   'cc_cvv': 'cvv'})response
 
-#outbound
+# outbound
 #   os.environ['HTTPS_PROXY'] = 'https://USi6vXVNrcsBYnpCMB7GciTg:2da52565-6e72-4a46-b419-245764f128ae@tntzrhiqrtg.SANDBOX.verygoodproxy.com:8080'
 #     res = requests.post('https://echo.apps.verygood.systems/post',
 #                         json={'add_message': message},
