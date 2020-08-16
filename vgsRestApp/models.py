5701f1763bff18bc6eaf8d5fb51618767544c1f9
BKT5031 <bkt5031@gmail.com>
2020-08-16 11:21:16 -0400

    New model id
    Cc_id add

--- vgsRestApp/models.py
+++ vgsRestApp/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 class creditCardInfo(models.Model):
+    cc_id=models.AutoField(primary_key=True)
     card_number=models.CharField(max_length=100)
     exp_date=models.CharField(max_length=100)
     cvv=models.CharField(max_length=100)
 
     def __str__(self):
-        return self.card_number + ' ' + self.exp_date + ' ' + self.cvv
+        return self.cc_id+ ' ' + self.card_number + ' ' + self.exp_date + ' ' + self.cvv
     
