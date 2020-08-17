0bb634ebe40070b9b72a4bcba3a2968fa6db1d36
unknown <bkt5031@gmail>
2020-08-16 21:57:28 -0400

    clean up code and readme.md

--- README.md
+++ README.md
@@ -1,11 +1,30 @@
+git clone https://github.com/BrianKTran/djangoRestVgs.git
+
+python --version # make sure you are using python 3
+
+pip install django djangorestframework requests
+
+python manage.py runserver
+
+Go to http://127.0.0.1:8000/index/
+
 Enter Sentitive CC Data:
         Credit Card Number 
         CC Expiration 
         CC CVV
-    Redact data
+    Hit submit to Redact data
+
+Hit refresh and hit ok to clear alert boxes 
+
+Copy tokens from The Data Table below and 
+paste to redact data form below
 
 Enter Redacted Data:
         Credit Card Number Token
         CC Expiration Token
         CC CVV Token
-    Reveal data
+   Hit submit to Reveal data
+
+Hit refresh and hit ok to clear alert boxes 
+
+See decrypted information in the Data Table below which has been sent to echo server
--- vgsRestApp/views.py
+++ vgsRestApp/views.py
@@ -18,23 +18,27 @@ from django.template import loader
 from rest_framework import status
 from django.shortcuts import render
 
-
+# View for rendering index.html template
 class index(APIView):
     def get(self, request):
         return render(request, 'index.html')
 
-
+# View for GET requesting all data available in database
 class ccGet(APIView):
     def get(self, request):
+        # get all objects from model
         creditCard1 = creditCardInfo.objects.all()
+        # pass objects to serializer to convert to json data
         serializer = creditCardInfoSerializer(creditCard1, many=True)
+        # set converted json to retData variable
         retData = serializer.data
+        # pass variable to return a response
         return Response(retData)
 
-
+# View for posting raw credit card data
 class ccPost(APIView):
     def post(self, request):
-        creditCard1 = creditCardInfo.objects.all()
+        # encrypt the raw data
         response = requests.post("https://tntzrhiqrtg.sandbox.verygoodproxy.com/post",
                                  json={"card_number": request.data["card_number"],
                                        "exp_date": request.data["exp_date"],
@@ -47,25 +51,24 @@ class ccPost(APIView):
         parseStr = json.loads(parsed['data'])
         # pass to serializer
         serializer = creditCardInfoSerializer(data=parseStr)
-        # save to post method
+        # save to local database to be viewed on index.html
         if serializer.is_valid():
-            # if True:
             serializer.save()
-        # print(parsed)
             return Response(parsed, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+# View for posting encrypted data to be decrypted into clean data to send to echo server
 class postReveal(APIView):
     def post(self, request):
-        creditCard1 = creditCardInfo.objects.all()
+        # decrypt tokens
         os.environ['HTTPS_PROXY'] = 'https://USi6vXVNrcsBYnpCMB7GciTg:2da52565-6e72-4a46-b419-245764f128ae@tntzrhiqrtg.SANDBOX.verygoodproxy.com:8080'
+        # post clean data to echo server
         response = requests.post('https://echo.apps.verygood.systems/post',
                                  json={
-                                      # "card_number": request.data["card_number"],
-                                      # "exp_date": request.data["exp_date"],
-                                      # "cvv": request.data["cvv"]
-                                       "cc_id": request.data["cc_id"]
+                                      "card_number": request.data["card_number"],
+                                      "exp_date": request.data["exp_date"],
+                                      "cvv": request.data["cvv"]
+                                    #    "cc_id": request.data
                                    },
                                  verify='vgsRestApp/cert.pem')
         # decode from binary string to regular string
@@ -76,11 +79,9 @@ class postReveal(APIView):
         parseStr = json.loads(parsed['data'])
         # pass to serializer
         serializer = creditCardInfoSerializer(data=parseStr)
-        # save to post method
+        # save to local database to be viewed on index.html
         if serializer.is_valid():
-            # if True:
             serializer.save()
-        # print(parsed)
             return Response(parsed, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
